1d54d288aa5f17b6b5a713cb6104fd24e86fd3d5
hazqeelknight <hazqeel.knight@gmail.com>
2025-09-09 15:48:59 +0500

    Fix Users Module Routing

--- src/users/routes.tsx
+++ src/users/routes.tsx
@@ -11,19 +11,11 @@ const AuditLogs = React.lazy(() => import('./pages/AuditLogs'));
 const MFASettings = React.lazy(() => import('./pages/MFASettings'));
 const RolesList = React.lazy(() => import('./pages/RolesList'));
 const PermissionsList = React.lazy(() => import('./pages/PermissionsList'));
-const PublicProfile = React.lazy(() => import('./pages/PublicProfile'));
-
-// Authentication pages (public routes)
-const VerifyEmail = React.lazy(() => import('./pages/VerifyEmail'));
-const RequestPasswordReset = React.lazy(() => import('./pages/RequestPasswordReset'));
-const ConfirmPasswordReset = React.lazy(() => import('./pages/ConfirmPasswordReset'));
-const ChangePassword = React.lazy(() => import('./pages/ChangePassword'));
-const RespondToInvitation = React.lazy(() => import('./pages/RespondToInvitation'));
 
 const UsersRoutes: React.FC = () => {
   return (
     <Routes>
-      {/* Main users module routes (protected) */}
+      {/* Protected users module routes */}
       <Route index element={<UsersOverview />} />
       <Route path="profile" element={<ProfileSettings />} />
       <Route path="security" element={<SecuritySettings />} />
@@ -33,16 +25,6 @@ const UsersRoutes: React.FC = () => {
       <Route path="mfa" element={<MFASettings />} />
       <Route path="roles" element={<RolesList />} />
       <Route path="permissions" element={<PermissionsList />} />
-      
-      {/* Public profile route */}
-      <Route path="public/:organizerSlug" element={<PublicProfile />} />
-      
-      {/* Authentication-related routes (these should be public) */}
-      <Route path="verify-email" element={<VerifyEmail />} />
-      <Route path="request-password-reset" element={<RequestPasswordReset />} />
-      <Route path="confirm-password-reset" element={<ConfirmPasswordReset />} />
-      <Route path="change-password" element={<ChangePassword />} />
-      <Route path="invitation" element={<RespondToInvitation />} />
     </Routes>
   );
 };
